e6059d25b4f248a886b0a8ae8e5612789283dfee
02deno <e190503029@stud.tau.edu.tr>
2022-05-01 21:06:45 +0300

    showPassword methodu eklendi

--- src/sample/LoginController.java
+++ src/sample/LoginController.java
@@ -3,18 +3,27 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.geometry.Point2D;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.time.Duration;
 import java.util.ResourceBundle;
 
+
+
 public class LoginController implements Initializable {
+
+    @FXML
+    private Tooltip tooltip =new Tooltip();
+
+    @FXML
+    private CheckBox showPassword;
 
     @FXML
     private Label loginMessageLabel;
@@ -55,6 +64,17 @@ public class LoginController implements Initializable {
             loginMessageLabel.setText("Invalid login.Please try again.");
         }
 
+    }
+
+    public void showPassword() {
+        Point2D p = passwordTextfield.localToScene(passwordTextfield.getBoundsInLocal().getMaxX(), passwordTextfield.getBoundsInLocal().getMaxY());
+        if(showPassword.isSelected() && passwordTextfield.getText() != "") {
+
+            tooltip.setText(passwordTextfield.getText());
+            tooltip.show(passwordTextfield, p.getX() + 340 , p.getY() + 200 );
+        }else {
+            tooltip.hide();
+        }
     }
 
 
